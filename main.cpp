5bb04161826b86b15692d215b8eca6c0baeae30f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-25 16:10:41 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -39,6 +39,9 @@ int main()
      * 
      * Sick minds can continue sicken each other, dont mind me appreciating the both "greatness and vileness" of
      * the universe that you all talk about so much as if you acknowledge what it entails 
+     * 
+     * Which "property" do you think you are regarded as ? :d
+     * 
      **/  
 
 
