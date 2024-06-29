f88dde6c89a5350c6f313f895156e9cec3eab097
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-30 01:26:35 +0300

    assd

--- main.cpp
+++ main.cpp
@@ -229,7 +229,8 @@ int main()
      * You just need to be around people like you and feel a little stimulation whether that be freeing from a guilt or something else through
      * totally bullshit means like cultivating a group that will say anything and feel great about your incomparable intelligence. Go on to your jerk offs
      * 
-     * Thats not how it is done. I have never been the one who wanted to be involved with such lowness on the scale as you 
+     * Thats not how it is done. I have never been the one who wanted to be involved with such lowness on the scale as you. As it is more than apparent
+     * every single time, you are there to demand you taken into consideration :d Fucking monkeys
      **/  
 
 
