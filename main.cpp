512c6cb0646d9dc51d80f575fa0e1201462f62ea
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-29 16:48:36 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -194,6 +194,8 @@ int main()
      * Dont stop watching stupid movies that are fictional and look harder around not to feel inferior
      * Your incompetent excuses will not cut it. You are effectively forcing someone change into something that you are miserably afraid of while
      * you are making claims. How inferior and stupid a group can get ?
+     * What sort of sick image have you generated as to believing that I will accept what is apparently unnatural over what I could have garner
+     * naturally ? Do you take your values to be so great ?
      **/  
 
 
