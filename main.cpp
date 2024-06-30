73375269187e57915d5c75a4f2281d4d8644498e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-30 21:15:45 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -125,7 +125,7 @@ int main()
      * 
      * I name you as the dumb clown monkeys of earth. That always brings about whatever they want gone 
      * You still ignorant of what you can not comprehend ? The lifetime of an individual ? What it could potentially entail ?
-     * You have zero perspective on that regard ?
+     * You have zero perspective on that regard ? Is this where your disgusting comfort coming from ?
      **/  
 
 
