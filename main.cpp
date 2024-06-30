e9d89ea0a8f05d7b26c61782996dc3ec2e99e3a0
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-30 16:59:50 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -91,6 +91,7 @@ int main()
      * others care so much as to regard "the past" as past. 
      * 
      * What are you still talking about ? How disgusting you are or just trying to alleviate as much as possible ? Yeah intelligence totally forgot about that :d
+     * I am telling you without you receive some sort of harm, there will be no other thing you can do. Thats just as simple as that
      **/  
 
 
