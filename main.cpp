bee48e0720c38ee9bcb4044135c5dc9c71f49713
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-30 19:49:06 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -108,7 +108,9 @@ int main()
      * 
      * Did you achieve it yet ? No ? Whats that you are trying to achieve anyway ? You still do not achieve ? Hmm thats definitely interesting :d
      * You are probably doing something wrong thats why you are not achieving anything :D In my opinion you have to first achieve your undisgusting sides 
-     * may be then you will start achieve whatever your aim in doing such bullshit as this that you can not even avoid properly :D
+     * may be then you will start achieve whatever your aim in doing such bullshit as this that you can not even avoid properly :D 
+     * What are you waiting for ? Are you contemplating unbeatable strategies against such a formidable situation ? Come on, you gotta achieve
+     * what can not be attainable go on. 
      **/  
 
 
