1d1bb7a95f59fd88c2aa47d3b6f37cff30f43457
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-06-30 21:03:31 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -117,6 +117,7 @@ int main()
      * 
      * Difficult decision ? A decision that you have no interest at all ? A difficult decision ? That is not your place ?
      * And an amount of time you have no control over has passed ? You still claim such decision ? What the fuck are you talking about ?
+     * You dumb disgusting people ?
      **/  
 
 
