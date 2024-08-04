829a25e5cba6fe33e9a775bceda2a093bdebbe25
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-08-04 23:48:14 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -21,24 +21,31 @@
 
 int main()
 {
-    /**
+    /** 
      * 
      * Event dispatching for Game class to further decouple and facilitate the transmission of internal properties otherwise
      * unamicable to perform so (Composition or inheritance(IExchanger like) ???)
      * Async environment for RemoteTerminal and RemoteStorages consider Disruptor for advancing purposes 
+     * Stateful joystick (indeterminate due to current command and future observer pattern implementation) to grant player 
+     * limited reversing ability (checkpoints to connotate). Requires mapping of opposite commands
+     * IsState queries in the game class dont comply with Open-Closed principle 
      * 
-    */
+     * You know it will play out just as the way you fear it would because your fears leads your decisions not REASON 
+     *   
+     **/     
+ 
 
-    InitWindow(500, 620, "Raylib Tetris");
-    SetTargetFPS(60);
-
-    // Grid* grid = new Grid();
-    // Level* level = makeLevelSingleton(2);
+    
+    InitWindow(500, 620, "Raylib Tetris");     
+    SetTargetFPS(144);               
+         
+    // Grid* grid = new Grid();  
+    // Level* level = makeLevelSingleton(2); <    
     // InputTerminal* terminal = new InputTerminal();   
 
     Grid* grid = new Grid();
-    Level* level = makeLevelSingleton(2);
-
+    Level* level = makeLevelSingleton(2);  
+  
     // BaseStorage storage = BaseStorage(); 
     // BaseTransformer formatter = BaseTransformer();
     // Exchanger exchanger = Exchanger(&storage, &formatter);
@@ -51,7 +58,7 @@ int main()
 
     GameScene gameScene = GameScene(game, joyStick);
     while(!WindowShouldClose())
-    {
+    { 
         BeginDrawing();
         gameScene.Draw();
 
@@ -61,4 +68,4 @@ int main()
 
     delete CommandFactory::GetInstance();
     CloseWindow();
-}
+} 
--- src/core/terminal/localterminal.cpp
+++ src/core/terminal/localterminal.cpp
@@ -3,43 +3,40 @@
 LocalTerminal::LocalTerminal()
 {   
     //Down button
-    this->clocks[0].SetInterval(0.032);
+    this->clocks[0].SetInterval(/**0.032**/0.0019999);
 
     //Right-Left button
-    this->clocks[1].SetInterval(0.1);
+    this->clocks[1].SetInterval(0.09);
 
     //Up button
-    this->clocks[2].SetInterval(0.5);
+    this->clocks[2].SetInterval(0.4);
 }
 
 int LocalTerminal::Capture()
 {       
     int pressedKey = GetKeyPressed();
-    if(pressedKey != 0)
+    double pressedAt = GetTime();
+
+    if(pressedKey != KEY_NULL)
     {
         this->activeKey = pressedKey;
         if(this->activeKey == KEY_DOWN)
         {
-            this->clocks[0].lastUpdateTime = GetTime();
+            this->clocks[0].lastUpdateTime = pressedAt;
         }
         else if(this->activeKey == KEY_LEFT || this->activeKey == KEY_RIGHT)
         {
-            this->clocks[1].lastUpdateTime = GetTime();
+            this->clocks[1].lastUpdateTime = pressedAt;
         }
         else if(this->activeKey == KEY_UP)
         {
-            this->clocks[2].lastUpdateTime = GetTime();
+            this->clocks[2].lastUpdateTime = pressedAt;
         }
-    }
-
-    if(this->activeKey != 0 && IsKeyReleased(this->activeKey))
-    {
-        this->activeKey = 0;
-
+ 
         return this->activeKey;
     }
 
-    if(this->activeKey != 0 && !IsKeyReleased(this->activeKey))
+    if(this->activeKey != KEY_NULL && !IsKeyReleased(this->activeKey))
     {
         if(this->activeKey == KEY_DOWN && this->clocks[0].HasPassed())
         {
@@ -53,11 +50,15 @@ int LocalTerminal::Capture()
         {
             return this->activeKey;
         }
-        else
-        {
-            return pressedKey;
-        }
     }
+
+    if(this->activeKey != KEY_NULL && IsKeyReleased(this->activeKey))
+    {
+        this->activeKey = KEY_NULL;
+
+        return this->activeKey;
+    }
+
 
     return pressedKey;
 }
