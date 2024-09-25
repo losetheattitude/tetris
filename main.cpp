824c37f2d976a9234f70e01ba43e11c09d5c0d31
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-25 18:24:16 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -172,7 +172,7 @@ int main()
      * 
      * ehehehhe the amount of stupid patterns you display... Certainly a strong indication of utmost intelligence
      * Only a slight feeling of "immature spoiled stupid kid" who can not assess unbiased information properly
-     * Hey look, we tryna help and fix something eheheheh, we competent ppl, talk about the violence
+     * Hey look, we tryna help and fix something eheheheh, we competent ppl, talk about the violence, talk about the conditions that you honorably pledged
      **/
 
     InitWindow (500, 620, "Raylib Tetris");
