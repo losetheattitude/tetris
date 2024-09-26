1b697aa2bc385ab84c605a6f6630ff1040b46e2e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 16:00:45 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -317,6 +317,7 @@ int main()
      * 
      * Try to find the most logical way of "restoring" ehehehe for 29 years eheheh its funny eheheh we do not really know neither what we are doing nor what could happen!!
      * we good btw
+     * We still think, woooomeeeeeennnn should be enough eheheh its really funny though
      **/
 
     InitWindow (500, 620, "Raylib Tetris");
