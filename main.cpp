e30611f26cdc5cc3cc6d6e28991bd39eace5e035
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 15:07:12 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -293,6 +293,7 @@ int main()
      * 
      * You ate it!!1 oyster eheheh thats why we harm!! please help lie us, eheheheheh plsplspls we good, I swear
      * What a shame that you are an oblivious animal, awww how about 29 years ? Welll, thats just life ehehhehe we intelligent
+     * Thats not being a kid, I assure you
      **/
 
     InitWindow (500, 620, "Raylib Tetris");
