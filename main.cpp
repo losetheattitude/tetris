3e29902c75beffcf983a5ab3ca14ad5950cb8a66
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 22:44:53 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -409,7 +409,7 @@ int main()
      * instead of begging, you could be doing a lot more other things, and you will get yourself into trouble
      * 
      * Are you fucking dreaming still ? Heyy we found some pieces, that should indicate so many things as to what we were doing!!
-     * And you still delude ?
+     * And you still delude ? and you still waste time ?
      * 
      * 
      * 
