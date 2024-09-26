e506a2c285a57de34a734d4e7f807d0fe6d042f4
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 15:44:45 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -309,6 +309,7 @@ int main()
      * Think about medications etc ehehehe, we tryna help we tryna solve world problems, Get some womeeeeennnnn!!! thats how intelligent we are,
      * we only see what we dream then get surprised at the possible outcomes eheheheh we just do not understand, we want enjoy our lifes!!
      * How come you take killing for a joke ? Warr!!!!! woooomeeeennn!!! get carss!!! ?
+     * Heyy, people tell us what to do!! we are not bad ppl !! plss lie for us!! you got 123123021 womeeeeen!!!
      **/
 
     InitWindow (500, 620, "Raylib Tetris");
