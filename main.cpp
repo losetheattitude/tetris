8577c71e126690423cd3ee5fa1ad7c2f8480fb53
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 13:00:30 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -271,6 +271,7 @@ int main()
      * 
      * You know there could be no going back, as you have alreay agreed eheheheh 
      * What could be the solution ? Womeeeeennnn!! we understand, we good, we tryna help, we solve problem
+     * Just try to explain your actions, and we can get to the talking afterwards
      **/
 
     InitWindow (500, 620, "Raylib Tetris");
