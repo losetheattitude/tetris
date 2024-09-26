db4dc52050029db49ef82665e9379698d3158082
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-26 22:11:47 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -399,6 +399,7 @@ int main()
      * 
      * Do the same mistake that others have done, do not be surprised at, or you would rather bend guising it with being "overly intelligent"
      * What you would love to call history, is not only really close but also one in which you harm irrelevant people
+     * You call it education, but guess what ? Have you ever been looked at the history ? You are just exactly the type in the history
      * 
      * 
      * 
