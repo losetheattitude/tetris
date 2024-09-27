862de1bc16be0b94469ec7c398c9a3edc1d57bd6
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 05:03:26 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -500,7 +500,8 @@ int main()
      * Look how you had a wonderful life because we were always there for you!! That means you can not definitely be the problem right ?
      * 
      * You still want that truth of your actions stay hidden ? Because then you would have to actually for once in your life upholded to your actions ?
-     * Ehehehe oyster!! he ate it!!! we are innocent!!!
+     * Ehehehe oyster!! he ate it!!! we are innocent!!! You can not have anything!! you can not go outside!!! you can not have money!! you can nott!!
+     * wait wait, please lie for us!!! we need your help to convince others and you to harm YOU!!! please lie for us
      **/
 
 
