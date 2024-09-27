12f3759bda568f83e8508899759d36ec9cd8b910
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 23:35:41 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -969,7 +969,8 @@ int main()
      * You are imploring me to act that reckless way but when it comes to explaining you say he does it on his own ?
      * You think you are not seeking for trouble ? and do you think that I am as simple as you are to
      * accept a few physical interactions for what you are failingly avoid to explain ?
-     * Whats the price now ? Just tell me the experience I had ?
+     * Whats the price now ? Just tell me the experience I had ? And correlate it with your past actions
+     * instead of calling it past, that is not past 
      * 
      * 
      **/ 
