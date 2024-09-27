281901cb0d256562621ea34c0d3dc96041196081
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 20:13:06 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -710,6 +710,7 @@ int main()
      * 
      * Dont be emotional pls!!! dont be emotional!!!! so that we can harm more to achieve the "nobody can see" because it does not exist ?
      * So is your irrelevancy, beg more take it for intelligence
+     * And still delude that you are trying to change something into something else, 30 years is nothing!!!! life is already over, and thats what we call intelligence!!!
      **/
 
 
