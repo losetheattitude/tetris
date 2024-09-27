9f679a752235de94481fd067209ddc09e80c066c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 17:51:53 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -574,6 +574,7 @@ int main()
      * 
      * All you can achieve is "using medicine and violence" ? Thats how simple you are, and you still say "you do it the old way" ?
      * Ignorant selfish old people, anything other than would not have made sense for such a disgusting group of monkeys
+     * Further eheheh
      * 
      **/
 
