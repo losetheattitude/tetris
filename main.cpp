5287bf759c1bdbc8f14276bb145e6f94fcceed2f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 20:29:50 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -742,6 +742,8 @@ int main()
      * we tryna teach people ehehehe good ppl btw
      * 
      * So you intentionally misguess and use that same inability to guess the outcome of 30 years, what could that be ? ehehehe fun life indeed
+     * Get cars!!!!! get womeeennnn!!! why not!!! life a simulation bro!! now you can get cars!!!!!! noooooo pls no, lie for us we need help of those we harm!!!!
+     * thats the intelligent part, stupid monkeys
      **/
 
 
