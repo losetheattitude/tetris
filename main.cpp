524f61d10897d5934d3ccbec1dc372d881000423
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 01:22:22 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1067,7 +1067,7 @@ int main()
      * So whether you think offering women and presenting it as gift would solve the issue ? Whats the issue again ?
      * Because I can not follow your logic ? Or are you just not making any sense other than HARM!! HARMM!!
      * And somehow you think you are not false ? Expect others to be harmed and be content with that, also talk about life
-     * but do not understand the individual you are facing.
+     * but do not understand the individual you are facing. That can not be the problem
      **/ 
 
 
