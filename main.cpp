52fb2c143927a40a9bcaa6b1c3bebe1ad26e21a5
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 20:16:00 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -715,6 +715,7 @@ int main()
      * 
      * Why are you still asking for random noises so that you could be considered what you are not ? Intelligent but you can not really explain ?
      * I do not accept any of you, whether you think you own the world, you seek for your own disasters
+     * Awww, you would rather beg right ?
      **/
 
 
