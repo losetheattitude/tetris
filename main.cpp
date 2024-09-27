ecb3e6b90bf308695e81baf12c7cc3f01f976e65
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 19:38:16 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -680,6 +680,7 @@ int main()
      * 
      * You misguess, misguess, misguess to the point that you have to accept but "THE PAST IS PAST!!!!!", need you some education bro ?
      * Brag, if you really insist to, with your own makings, do not interrupt contexts that you have no rights over whether you think you are saving the world
+     * If you do not follow, that is also intentional and all that bullshit okay, you are just right, talk about the past, a few years back only eheheh
      **/
 
 
