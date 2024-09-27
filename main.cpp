4e296c43bff3e6ed6c5e46a5f339cfee82cc695b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 19:47:56 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -689,7 +689,7 @@ int main()
      * Fix that before you interact with others, or you would rather act like its fine, although its the most disgusting thing you could ever commit. Right ?
      * 
      * Awwww, competent people tryna solve problems eheheheh the problems waooww, impressive 
-     * Who is the problem again bro ?
+     * Who is the problem again bro ? We tryna harm and we do it for the greater good eheheheheh intelligent ppl btw
      **/
 
 
