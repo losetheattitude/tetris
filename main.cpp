807d9ada46c3d604dd73f458402bd93d70d7a8fc
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 17:54:51 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -578,7 +578,7 @@ int main()
      * You would call cheap intelligent because of why ?
      * You would dare calling what is the most cheapest form of actions intelligent, because you are a cheap stupid animal ? Isnt that correct ?
      * You have had all the opportunities of doing what you say you would ? but you still beg when and guise with intelligent ?
-     * That has to be cheap right ? Who have I been with so far ?
+     * That has to be cheap right ? Who have I been with so far ? and what did you end up doing ?
      * 
      **/
 
