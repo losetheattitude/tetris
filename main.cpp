cce73a97211de5fca8eaccf2eba9946072d4da8c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 20:26:14 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -733,7 +733,7 @@ int main()
      * 
      * You dare offering cars ? with your narrow point of view ? you dare offering what is available ?
      * But you would also love practicing your great narrow point of view to apply some violence ? and you say you are not an animal ?
-     * You can not use your brain ?
+     * You can not use your brain ? Talk about the violence
      **/
 
 
