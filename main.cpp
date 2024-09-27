156c09802b0ce3a7a1ee3c7f94ac63572be017a1
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-27 22:40:27 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -905,7 +905,7 @@ int main()
      * 
      * Are you still after the same unjustifiable actions ? Wait wait, the same wise people offer material all of a sudden ? Calling it fair game or life ?
      * or just fate ? what are you talking about ? You still insisting on your stupid claims ?
-     * Where is the proof other than your coercions btw ?
+     * Where is the proof other than your coercions btw ? He did it!! he did it!! medicine bring more medicine btw
      **/ 
 
 
