90f1d99c0a681adb7295c251553be67494c3bcd6
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 21:05:11 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1312,7 +1312,7 @@ int main()
      * Can you tell me the experience I had under the light of your "great noises" ? Just try to use your brain on reason, you will see you are false with every single
      * attempts of malign intention, thats why you need education eheheh
      * so you like hurting people who can not defend ? How come you think it should be FATE ? when the group of stupid animals are doing their best to harm ?
-     * YOU SAY YOU DO IT THE OLD WAY ?
+     * YOU SAY YOU DO IT THE OLD WAY ? Great outcomes, great people, great reasons, and you can all get the fuck out of my face
       **/ 
 
 
