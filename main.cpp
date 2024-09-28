57e85123f430f03eca51ea449c6d341f211260ee
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 22:09:27 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1422,6 +1422,8 @@ int main()
      * You get sentenced for your own actions ? As opposed to actions that you coerced, for 30 years
      * Take violence for an education, thats why you are animal
      * 
+     * You think its funny or normal ? for someone to waste 30 years on false terms ?
+     * 
       **/ 
 
 
