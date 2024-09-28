83227f26c59292f28d2c6f537f4ef0ab2603dbf5
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 22:54:43 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1471,6 +1471,8 @@ int main()
      * We still think we are right in our coerced actions, thats how reason works ?
      * You get violence or get the proper sentence for your actions and you see, when everyone is happy, no problems occur
      * 
+     * WHY ARE YOU STILL BEGGING ?
+     * 
       **/ 
 
 
