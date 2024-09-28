4b137b0b974c465c048e89693f532d9f5b98c96e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 23:35:46 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1496,7 +1496,7 @@ int main()
      * So you had all the chances but you for a reason have not acted on them, but now you say, you are credible and you would realize it ?
      * Wait wait, now you demand credibility for your inexplicable actions ? Say something logical, or honorable, or good or provable or NOT COERCED ?
      * 
-     * How come you think you make decisions ?
+     * How come you think you make decisions ? and start begging like the animal you are once you are confronted with your INTENTIONAL harms ?
      * 
       **/ 
 
