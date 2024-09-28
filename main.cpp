b722280dc63662758f68f974fcc841a58a3dcccd
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 21:18:06 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1318,6 +1318,7 @@ int main()
      * And you think I would want to have anything to do with you ? Ehehehe get some womenn ?
      * And you also claim that you are trying to help while you have so many chances of doing that ?
      * We coerce kids into things so that we can call them certain names, so that later on we can present it as proof to not allow them talk ?
+     * Whatever brooo, get some women!! wait we already made that promise countless times right ?
       **/ 
 
 
