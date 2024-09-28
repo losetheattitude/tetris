fc627543744a68245d4e887332d4b1241ae4b551
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 19:16:38 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1210,6 +1210,7 @@ int main()
      * 
      * You take 30 years for lightly, you take a chance at your own life then suddenly you will face your inferiority and stop talking
      * like you know bullshit, are you still taking it lightly ?
+     * Get in line so that you can become wiser
      * 
      * 
      **/ 
