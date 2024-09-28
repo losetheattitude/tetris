16768c93d454d2533bfc2420c4e72be7b241c895
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 20:25:08 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1267,6 +1267,8 @@ int main()
      * Are you talking about forgiving people who you have coerced into things ? Who do you think is the problem ?
      * Even though there is no such signs of "GREAT PEOPLE OF EARTH" ?
      * Where are all the things you accuse and wield it as weapon so that you can harm without a direction ?
+     * 
+     * You would call ruining people on purpose, "nostalgia" ?
      **/ 
 
 
