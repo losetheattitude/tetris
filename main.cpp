8152d1b1ffb22fbe29c265f5b13568ea596cea1b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-28 19:23:01 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1211,7 +1211,7 @@ int main()
      * You take 30 years for lightly, you take a chance at your own life then suddenly you will face your inferiority and stop talking
      * like you know bullshit, are you still taking it lightly ?
      * Get in line so that you can become wiser,
-     * wait, you do not risk anything but also ask from others to do so ?
+     * wait, you do not risk anything but also ask from others to do so ? and you offer womenn ? although you do not deliver ?
      * 
      * 
      **/ 
