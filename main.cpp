d9cbb72643208de7448556afa835d8347c2eaa1b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-30 02:26:00 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1869,6 +1869,7 @@ int main()
      * Btw, he just acting eheheh we tryna help, ehehe we intelligent ppl, we give women btw
      * So far, you have tried to educate people and somehow you are failing to receive the reaction that you are seeking ?
      * Or is this the reaction you are seeking because you know you are guilty ?
+     * No no, not the dumb animals btw
      *  
       **/  
 
