102d044c584555641c666af4fc142b8902e41d13
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-29 09:48:52 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1771,7 +1771,7 @@ int main()
      * You do it the right way or old way ? or you just educate through violence ? or you just make promises ?
      * Or as I have said, you are only looking to enjoy your experiences so you offer what you value ?
      * 
-     * So you are saying, you do it the old way, but you also do it the right way, but you say its the only way, you also say get womeeenn!!!
+     * So you are saying, you do it the old way, but you also do it the right way, but you say its the only way, you say violence is education, you also say get womeeenn!!!
      * you also say, past does not exist ? I only say one thing, tell me your credibility ?
      * Now try to explain it one by one, why you think you are right or how come you think you make decisions ?
      * 
