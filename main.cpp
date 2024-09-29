270586a1915d1e179507cee815c4698817a65c1c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-30 02:19:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1862,6 +1862,7 @@ int main()
      * We wants to act like we know life, and start harming suddenly btw eheheh
      * Please lie for us eheheheh, wait wait, why are we waiting in the first place ? It has been months and we are still waiting for something to happen eheheh
      * If you lie, we give you women btw ehehehe its so fun to be intelligent ehehehhe
+     * Wait wait, you was gonna say, you tryna educate people eheheh
      *  
       **/  
 
