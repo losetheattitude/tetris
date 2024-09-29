a0940202d2700a26874d4f9f8aa034d22832ab1c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-29 09:27:15 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1760,7 +1760,8 @@ int main()
      * You object to truth, why is that ? You also say, you understand life, time does not exist, and you do it the old way ?
      * And you still allege you act with reason ? but all you can say is complete bullshit that contradict eachother ?
      * Why is it that though you suddenly are inclined to play "stupid childish games" to save your experiences from ruining which they should already be ruined ?
-     * You love objecting truths that threathen your precious experiences while you talk about "being wise on life" ?
+     * You love objecting truths that threathen your precious experiences while you talk about "being wise on life" ? Time does not exist btw,
+     * Get some women btw ?
      * 
       **/ 
 
