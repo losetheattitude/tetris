0c5b11ac60f3e7c41608a14302972de0b8ddf937
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-09-30 22:27:40 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1931,6 +1931,7 @@ int main()
      * Education we seek, education eheheh
      * 
      * I havent done anything other than what you have coerced kids into, be proud about that as well, not dumb disgusting monkeys
+     * Now tell me my experience 
      * 
      * 
       **/  
