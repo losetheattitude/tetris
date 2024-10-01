5a136ecf6d000cc48d35fe198e1ce1d6ec738745
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-01 07:36:08 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2188,7 +2188,8 @@ int main()
      * I see, thats not the problem btw yes
      * Awwwww, you also say, get womeeen!!! get carss!!!! get money!!!! life a simulation btw, life short, life good, we good, we just, thats right yesss
      * Beg more, may be you convince you re not inferior
-     * Btw, good night etc,
+     * Btw, good night etc, Awwww, you also provide inferior excuses for your inferior actions that creates inferior circumstances for everyone ?
+     * You say you good etc ?
      * 
      * 
       **/  
