8bcd87f65df0ad35913d3533dd38eb01d5d47df3
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-01 07:46:15 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2198,7 +2198,7 @@ int main()
      * Awwwww, 30 years you say ? How about that ? Awwww, you say its fate!!! its life!!!! its simulation!!! What a shame that you are a disgusting animal
      * 
      * Awwwww, you say its education because you are afraid of violence as the little animal you are ? And you misguess that it would help scare others
-     * that you do not understand ? Who is sick btw ?
+     * that you do not understand ? Who is sick btw ? Awwww you are just lost animals, thats the problem, you are afraid as the little animals of earth
      * 
      * 
       **/  
