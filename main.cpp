591b16099714273b2b431fcf0dd7257dd1520dd7
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-01 07:33:58 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2184,6 +2184,9 @@ int main()
      * Wait wait, when was the last time you were begging again ? You do not get to make decisions with your malign contexts
      * Wait wait, whats the problem again ? Awwww, you are too afraid to deal with what you can not match because you are too inferior ?
      * 
+     * Awwww, thats the problem, you too inferior so you seek in your own nature
+     * 
+     * 
       **/  
 
 
