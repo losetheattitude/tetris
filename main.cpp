92f8009dccaf8c2a7048af04c823f13e80846fb1
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-02 00:38:02 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2309,6 +2309,7 @@ int main()
      * Get womeeeennnn!!! get carsss!!!!
      * 
      * You keep saying, go jump into cars as if you have credibility ? Take this, jump in this car, get with this people, we tryna help, we always have eheheh
+     * Awwww, its too late now we can only but reject!!! and keep harming, hopefully nothing goes wrong
       **/  
 
 
