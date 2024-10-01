92982a16fe52de7b4a896b24c04818bd16a3db23
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-01 23:04:51 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2280,6 +2280,9 @@ int main()
      * 
      * Awww, you would rather call it insane ? Eehehe good ppl, you have no other choice other than to act on your animal side and regret it over and over again 
      * Great ppl btw, Where are you btw ? Are you watching the little animals ?
+     * 
+     * I have no business with the people like you, do you still not get that you are a selfish stupid animal ?
+     * I do not even want to spend one second with you
       **/  
 
 
