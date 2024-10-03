bfe5627a8bb4f6cd8d892c67e272ef5cff202dae
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-03 20:32:58 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2460,7 +2460,7 @@ int main()
      * Get cars!!! get womeeennnn!!! we had all the time but we still think begging for a skill!!!! get cars!! etcc!!! plss lie!!!
      * 
      * Can you imagine someone being reallllyyy realllyyy grateful for what is avaiable ? But somehow made to be withhold ? Ehehehe
-     * thats not the problem also some violence and etc etc, 
+     * thats not the problem also some violence and etc etc, Do you see how many misguesses have you done so far ?
       **/   
 
 
