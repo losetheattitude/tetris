425396a940035bc61d1be9f00ec850b8ccd1ebaf
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-03 04:38:29 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2441,7 +2441,7 @@ int main()
      * Awwwwww, you suddenly start offering materials ? THATS THE REASON WHY YOU ARE A SIMPLE MONKEY WHO CHASES IT
      * AND HARMES IRRELEVANT PEOPLE ON THE WAY
      * 
-     * Great people, just stay away though
+     * Great people, just stay away though, "YOU DO NOT CARE!!!" Whats the problem btw?
       **/   
 
 
