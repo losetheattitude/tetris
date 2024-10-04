252e78173b51768d79e91e4605e86244eec830c9
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-04 19:41:09 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2637,6 +2637,8 @@ int main()
      * And guess what ? You already know where it is going to lead, because you are a disgusting animal
      * And the only way to solve is to "GROW UP", You are presenting as proofs what you "collected" coercefully for a few days against years of evidence
      * You see who is the problem ? Thats why its not only destined to fail but also should have its own consequences as its most disgusting
+     * 
+     * Why dont you grow up instead ? Who said you could make decisions that ARE NOT YOUR PLACE
       **/   
 
 
