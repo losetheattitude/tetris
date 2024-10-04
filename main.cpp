48cc4980f99d0922790d84722901cea8e63f712f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-04 20:19:48 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2658,6 +2658,7 @@ int main()
      * 
      * So you are not animal you say ? And you still think what you present as education to others, you do not welcome it ?
      * And you hide behind incompetent excuses as you yourself is incompetent at your "jobs" ?
+     * Better use your brain next time
       **/   
 
 
