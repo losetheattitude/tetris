6cabf3581fba157bc59de4f5184a3142228e6829
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-05 00:20:14 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2694,6 +2694,7 @@ int main()
      * Awwww, you cop, you this, you that, please we want no losses eheheh thats the truth that you can not distinguish from your stupid emotions dumb animals
      * 
      * Do you remember the conditions ? You stupid stubborn ignorant selfish people ?
+     * You keep saying mean, but you do not recognize your inexplicable actions ?
      * 
       **/   
 
