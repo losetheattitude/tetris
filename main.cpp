de2a026cb144bdd0bcd06e2f6a4172cc339606a8
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-04 22:36:48 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2682,6 +2682,7 @@ int main()
      * as you have demonstrated it with every decision that is not your place
      * 
      * You are irrelevant!!! eheheheh its funny though
+     * You said fair game regardless ?
       **/   
 
 
