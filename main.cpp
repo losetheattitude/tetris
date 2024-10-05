a4c46f8026ae611c9ca0f74f62bef78e3411424f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-05 12:29:56 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2725,6 +2725,7 @@ int main()
      * 
      * You already know, you are %100000000000000000000000 guilty, and there is nothing to talk about
      * Not even a single thing anyone can say from both sides, so better get the fuck out of my face and pay for your own actions
+     * Wooooomeeeennnnn!!!
       **/   
 
 
