cf278108201410ce9e2e1dfd5a7b3575afc5420f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-05 17:40:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2805,6 +2805,7 @@ int main()
      *  
      * You still making offers ? 30 years ? eheheh funny stuff right ? Awww you still go against years of evidence with a few days of coercion eheheh
      * And following that approach, you suddenly start making offers ? Where were you ??? eheheh funny btw
+     * Get woomeeeennnn!!!
 
      * 
       **/   
