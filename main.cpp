4107e52dafdd2f0929ac571d2c7d61762e2c74c2
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-06 02:00:02 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2885,7 +2885,9 @@ int main()
      * 
      * Aww you say, you trust now ehehe until you free yourself from your crimes ? eheheh inexplicable etc
      * greater good btw, I am not interested in anything you could possibly form, as the "history" has proven on multiple occasions
+     * 
      * If you did your job then and now and back then, we wouldnt have any issues, do you see the pattern ? How about start listening instead of ignoring ?
+     * Because I can really tell when someone does not know what he is doing
      * 
       **/   
 
