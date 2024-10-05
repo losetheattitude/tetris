a60b8d0dfb40c0bdb08725553b5b09b0d2ed9416
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-05 17:35:08 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2800,7 +2800,8 @@ int main()
      * Do you remember the coercions etc ? eheheh I am sure thats not why you feel the need of being sad!! funny stuff indeed
      * You know why you are begging right now, its because your nature is exactly that inferior, delude about that as well
      * Awwww, you are also proud that you manipulate people who has no proper choice ehehehe so fun btw
-     * Why are you still begging through "offers that your unseen honor have never delivered" ?
+     * Why are you still begging through "offers that your unseen honor have never delivered" ? Thats why you are inferior, also the fact that you enjoy 
+     * your inferiority eheheh funny stuff
 
      * 
       **/   
