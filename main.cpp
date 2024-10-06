60abc1ae0db9f7634d8f28e96987c6720a21fe45
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-06 20:48:05 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2901,6 +2901,7 @@ int main()
      * If it wasnt working, it would have never in the first place
      * Awww, you thought it was a game, thats why you take life lightly eheheh
      * Thats being unreasonable, you still say its not "childish" games what you are playing ?
+     * But still allege that its justifiable hehehe good ppl
      * 
       **/   
 
