39c526def4b64a730abf4e1c3748e7b7e12ea1f6
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-07 01:46:44 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -2927,6 +2927,7 @@ int main()
      * Incompetent people still thinking that acting as if it should be okay makes it okay
      * Awww, you were the type that takes violence for a skill 
      * Please please, can you lie for us, fix this once more, we need it so that we can enjoy our precious experiences!!!!
+     * No no, not obsession indeed
       **/   
 
 
