82b6cf0e7469bcab8244ed7368737e6d36025fd1
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-07 22:16:37 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3088,6 +3088,7 @@ int main()
      * then harm more once you feel safe ? That is not insecurity ? eheheheh harmmmm plss harmmm we seek harm, eheheh funny stuff
      * 
      * Wait wait, tell me once more, instead of acting on, why you wait and look around imploring ? Awwww, its because you are entirely false and guilty ?
+     * And you think I will fear like the little animal you are ?
       **/   
 
 
