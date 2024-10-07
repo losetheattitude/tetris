cc2b6f67e633fb5690d3df8a345020d0f59a95b3
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-07 21:48:55 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3062,6 +3062,7 @@ int main()
      * Awwww, but you still think you should make decisions, eheheheh wait wait, please lie !!!!
      * 
      * Wait wait, you still enjoy your selfish experiences while advicing otherwise ?
+     * Awwwww, you still say life ?
       **/   
 
 
