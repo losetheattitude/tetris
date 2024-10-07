497a25d0694ba4b2a2b4fede934b4b485a056e97
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-07 21:37:05 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3057,6 +3057,7 @@ int main()
      * 
      * Do you still need help with something you great people ? eheheheh you religious etc eheheh really really fun stuff
      * Heyyyy, harm more eheheheh we can do it ejehehe why not ? You intelligence I see fucking dumb monkeys, reduce to begging thats who you are,
+     * Ignorant not btw eheeh
       **/   
 
 
