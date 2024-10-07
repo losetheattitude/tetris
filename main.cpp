2e7cee0596f706e27e8627c49b772f15400a6be3
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-07 21:58:44 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3072,6 +3072,7 @@ int main()
      * 
      * Thats the truth I guess, heeheh, are you still talking about competency ? You get carss!!!! womeennnn!!! good life!!! pls help lie
      * Wait wait, how do we deal with such a "truth" that we have formed ? If it were to come surface ? pls pls, help lie us so that we can harm more!!!
+     * The term, stupid disgusting people is coming from just that, you still allege ?
       **/   
 
 
