56273e51b238f8aee556b185f5359d41c30e24b3
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-08 03:35:22 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3147,6 +3147,7 @@ int main()
      * shhhhhh, do not make sound!!! pls, we need you to remain silent eheheheh we winnin btw
      * Why though you still waste time ? You take me for stupid as well ? Awww I do not know who I have had relations with eheheh funny
      * You think I can not recognize ? Thats why you inferior with all your "etc etc"
+     * Your not seeing it proves it more and more, thats acting insufficient, you should have already known that
       **/   
 
 
