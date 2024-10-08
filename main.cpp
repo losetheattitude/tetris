b1430b7f54b64c28c4025380fc5c31a662698311
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-08 20:17:20 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3171,6 +3171,7 @@ int main()
      * Wait wait, talk about violence and your genius correlation with education and think about "why your education is not working ?"
      * because you have mistaken me for your inferior selves ?
      * Talk more about violence ehehehe you can see why, so you still mistaking ?
+     * Awww, you still make up rules ehehehehe really funny stuff
       **/   
 
 
