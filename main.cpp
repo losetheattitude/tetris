6729e3eca99e9a5b89e3b7501f4e3a579bddcdf6
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-09 00:52:35 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3228,6 +3228,8 @@ int main()
      * we wait eheheh why dont you come eheheheh we good ppl ehehehe pls help us lie eheheh
      * 
      * Its not like, "if we had the intention we would have already acted on so "I am like, yesss definitely good ppl", but Imma pass on them disgusting animals  "
+     * Awww, I see, you just want to enjoy your experiences and do not want any sort of harm ? ehehehe but you would like to keep it for others eheheheheh
+     * good religious ppl indeed
       **/   
 
 
