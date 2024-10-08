51e7a47892c661e4a2e2dfddbba08d01b76db1dd
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-09 01:39:24 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3330,6 +3330,7 @@ int main()
      * 
      * Obviously you will be the one who will say, "FORGET ABOUT THE PAST PLEASE!!!!", why though am I supposed to be the gentle one ?
      * Take a look at your disgusting nature, what do you have to say ? "GET WOMEEENNNNNN!!!!"
+     * Help us fix it!!! please!!!
       **/   
 
 
