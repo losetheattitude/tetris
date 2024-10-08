9a90b5daf6d29d00b8b7d179588843528c2c6b35
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-08 23:18:12 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3198,7 +3198,7 @@ int main()
      * Its too late!!! eheheh great achievements btw, thats education ehehehe really fun 
      * 
      * Awww, you also suddenly think that I should care ? awww you say "its tooo lateeee!!"
-     * Awwwwwww, its too late!!!!
+     * Awwwwwww, its too late!!!! Awwwwww, you have forgotten what life is ? eheheh its too late!!!!
       **/   
 
 
