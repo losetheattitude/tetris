139ca045564e2c32bb46f6c13839f3d4bc78a735
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-09 21:58:01 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3586,7 +3586,7 @@ int main()
      * 
      * You do not get to decide, and thats all, ehehe 
      * btw we can not deal with that so we better start accusing!!!
-     * and you should also be silent ehhehe
+     * and you should also be silent ehhehe PLEASEEE!!!
      * 
       **/   
 
