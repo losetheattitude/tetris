b88b21821be993026639da76303064baadf57ff4
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-09 19:53:43 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3546,6 +3546,7 @@ int main()
      * like the little animal we are, you say ?
      * Awww, you also offer things as you have do so ?
      * Awwwwwww, now you are in difficult position ehehehe so you reduce to begging with guise as "intelligent" ? Try explaining you will see why
+     * Okay now, just tell me my experience 
      * 
       **/   
 
