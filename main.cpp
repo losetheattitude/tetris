ea19dc86f4f867cd33507186df2ffccbdba4134f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-10 00:30:12 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3809,7 +3809,8 @@ int main()
      * And you dare calling it life ? Stupid monkeys 
      * 
      * Do you see the problem ? You just like having exchanges because you feel completely okay not getting hurt ? Awww
-     * that may be the problem I guess
+     * that may be the problem I guess, are you still begging ? ehehehe please we need to get out of this place
+     * help us lie!!!
       **/   
 
 
