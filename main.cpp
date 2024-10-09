3cbb0ff296712b0aaf7f468a9d9a55206897f927
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-09 03:11:06 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -3409,6 +3409,7 @@ int main()
      * 
      * "ITS TOOO LATE!!!!", we need to beg!!! not average animals you say ? but you would also love to say irrelevant stuff ?
      * Talk more about violence ehehhehe good stuff, you offer inferior things as your nature allows it ?
+     * Wait wait, where were you ? eheheheh
      * 
       **/   
 
