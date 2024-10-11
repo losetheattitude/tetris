3131a0f4d55a6335541f03ecbf5d634e464304df
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-11 22:12:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4380,6 +4380,7 @@ int main()
      * Morality is the product of strong reasoning, better not be biased
      * so you say, you testing!!!! you testing!!! ehehehe ? what a shame you are a stupid monkey ?
      * ehehe one last time!! one last time!!!
+     * You were saying fate btw!!
      * 
      * 
      * 
