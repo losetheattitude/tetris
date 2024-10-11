075db52db2d712a42ec11493ff6b85dca3e8ce6c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-12 02:47:11 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4657,6 +4657,7 @@ int main()
      * USE YOUR BRAIN BETTER NEXT TIME, AND USE IT TO GREATER GOOD AND ONLY FOR YOUR OWN LIFE
      * But you see the problem right ? The ignorance it means
      * Do you still believe I was joking about "COERCION OF MINORS AND SO ON AND SO FORTH ?"
+     * But do you see the ignorance right ?
      * 
       **/    
 
