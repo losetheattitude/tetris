1c7ae3a03c43f22eca71b111f795a8c95872369c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-11 10:07:02 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4250,6 +4250,7 @@ int main()
      * we good ppl!! plss
      * 
      * Are you still offering women and also threatining at the same time ? Religious people you say ?
+     * Are you still "alleging" that you are building ? Are you still the same disgusting animal ?
       **/    
 
 
