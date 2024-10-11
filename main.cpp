fe8f9c92d846ed3ce05a4f1613957c09d42a770c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-11 06:19:59 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4166,7 +4166,7 @@ int main()
      * 
      * 
      * Continue begging for your experiences, still accusing ? talk about the violence dumb monkeys eheheh
-     * No resources!!!! please lie! lie! forgive! ehehe
+     * No resources!!!! please lie! lie! forgive! ehehe, violence!! lie!! lie for us! pls!! forgive, immature!! pls!!
       **/    
 
 
