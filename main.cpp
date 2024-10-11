03b3377bfee31e74da89f6a61c274e6273a2081e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-11 09:12:08 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4200,6 +4200,7 @@ int main()
      * And now you are still asking things ?
      * 
      * And you still offer things ? Heyyy you get women!!! you get womeen!!! please lie! lie!!
+     * Now you feel like you are in trouble because you have done what you shouldnt have, and started asking things ?
       **/    
 
 
