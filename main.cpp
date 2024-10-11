97d520d1dcde1aa9ff048f5b75de00b462f11b82
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-11 04:47:34 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4141,6 +4141,8 @@ int main()
      * 
      * You still after the RIGHT THING TO DO ? Read some bro ? Read and see what people think about the inferior animals like you
      * I am sure you will avoid that as well, because of your inferior nature and start begging for lies as thats your NATURE
+     * 
+     * You see the problem
       **/    
 
 
