416ff32223f5306a1e30a9875c9cd2d546a5ed78
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 00:03:40 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4986,6 +4986,8 @@ int main()
      * 
      * Ehehehe its education, although there is no reason, we just like to make up reasons because we are little animal who can not account for his own behavior
      * Awwww, thats why you can not deal with truth and start making up excuses as I can tell from all the EXPERIENCE that I had with the little animals like you
+     * 
+     * The little animals who take a few days of stupid game coercion over years of evidence. Ehehe fun though right ?
       **/    
 
 
