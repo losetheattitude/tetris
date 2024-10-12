e2fdc3ad72db41bcead490b168a6dbe350c58387
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 01:27:48 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5011,7 +5011,8 @@ int main()
      * And you offer what now ? YOU SAID, NO RESOURCES!!! and what did you end up gaining now ?
      * 
      * And you still ALLEGE THAT you were there and watching while the time has long gone with countless inexplicable actions were being committed ?
-     * And you ask for what ?
+     * And you ask for what ? Do you still claim you understand life or time ? How about the time that has gone and the "education" that should be 
+     * received is also "unattainable" ?
       **/    
 
 
