a7d1d2d31a2e8d138f9c7617a22eec0f0a11dced
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-12 03:48:24 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -4744,7 +4744,8 @@ int main()
      * You are the problem ehehehe
      * But do you now see the problem ? Do you think you are a good "men" as you would love to delude as well ?
      * You lack every single attribute for a man, only some violence and disgusting delusions, and those do not make you a man, you are just a begger
-     * Here we are, you are begging again,
+     * Here we are, you are begging again
+     * and you dare calling it useless ?
      * 
       **/    
 
