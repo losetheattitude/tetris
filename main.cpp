04cb80513fff9f9e4b50a0427d02286214bba3f0
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 03:33:20 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5088,7 +5088,8 @@ int main()
      * 
      * When you had all the opportunity of "performing" what you now are alleging that you will ? Are you fucking dumb ?
      * Just talk about the past times and try not to remind yourselves that "ITS TOO LATEE!!!!" because I already know and remember every single detail
-     * and will make you remember as well ?
+     * and will make you remember as well ? Keep you also remembering all those details ? You beg to avoid ? 
+     * Are you still offering women ?
       **/    
 
 
