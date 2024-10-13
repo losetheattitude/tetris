0367f33f0b898590823f0351259b37150cb6cb0b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 22:30:44 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5150,6 +5150,8 @@ int main()
      * eheheh, its the only way! please help us lie and make money!!!
      * Is this the time where you say, "we need think something for this or that ?" and you come up with "womeennn" as you have done so ? Very very clever
      * And you still delude you should make decisions
+     * 
+     * You still suggest as if it is a rule ?
       **/     
 
 
