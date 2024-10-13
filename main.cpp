15fbf1b09e9d457e3b9e7f8250868743cdd906c8
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 03:44:48 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5111,6 +5111,7 @@ int main()
      * You know you just get that
      * 
      * What are you saying ? You making offers still ? I am saying "time" and "experiences" ? What are you saying ? I JUST WANT TO HARM!!!!
+     * ehehe please say this for us!! do this for us!!
       **/     
 
 
