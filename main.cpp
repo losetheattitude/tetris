364670dab1907852cde0219805c8cfadb1a08eff
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-13 08:08:59 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5135,6 +5135,7 @@ int main()
      * Are you still offering women ? after years ? eheh honor!!! virtue!!! delusion!! pls do not mention!! pls lie!! 
      * life short!! life simulation! time illusion!! I said I remember everything you still say "YOU DID IT!!!!"
      * You have to do this!! you have no option!! violence!!! as your great honor and virtues can prove that you are the type, delude as you will
+     * Of course you will be the one who would rather forget ABOUT THE PAST!!!
       **/     
 
 
