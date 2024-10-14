d7e54a8c35d7c3527b1fd5fc37cf2925f0b7d617
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-14 06:03:40 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5199,6 +5199,7 @@ int main()
      * 
      * Do you still think you should make decisions or offer advices or make really credible offers ? TALK ABOUT 30 years you will see why you are wrong
      * Are you still offering women ?
+     * Most disgusting indeed
       **/     
 
 
