6b55962a4829962fd5d4524d53d8ee21853ec5e3
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-14 06:48:59 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5240,6 +5240,9 @@ int main()
      * 
      * What you call superior skills I call being inferior liars ? What do you say ? ITS TOO LATE!!! we achieved!!! eheheh ? 
      * WHAT DID YOU ACHIEVE BRO ? You say "please be silent" ?
+     * 
+     * Awww, you would hate to take lightly when its your life ? But its entirely fine when it comes to others ?
+     * That sounds like a major problem
       **/     
 
 
