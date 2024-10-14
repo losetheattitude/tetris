8c951f7d57e824cd692115e3d49b13c363fc2907
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-14 07:59:17 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5274,6 +5274,7 @@ int main()
      * You like harming with false precept ? but also ignore the evidence ?
      * And you are still looking around to see if anything changed or your stupid education worked ? what a disgusting animal you are, fun stuff
      * Awww, you also add to your education "NO RESOURCES!!!" and you see why you are a disgusting monkey who likes to harm guising it with all sorts of bullshit
+     * Talk more about violence and correlate your "excuses" 
       **/     
 
 
