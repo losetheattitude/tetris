31649ac3f42c6174c524968d79790b115d98149e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-14 06:47:21 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5239,6 +5239,7 @@ int main()
      * Life is gone, no friends ? no time ? no resources ? what do you say ? ITS TOOO LATEE!!! No possessions ? You say its too late!!!
      * 
      * What you call superior skills I call being inferior liars ? What do you say ? ITS TOO LATE!!! we achieved!!! eheheh ? 
+     * WHAT DID YOU ACHIEVE BRO ?
       **/     
 
 
