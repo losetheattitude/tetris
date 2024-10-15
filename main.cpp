8b12f3d78e42b872ace5ea64928aafe22a580103
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:33:31 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5369,6 +5369,8 @@ int main()
      * So you still are not inclined to see that you CAN NOT ENFORCE anything on nobody, but you would just like to hide that you have done so ?
      * that you have such and such inclanations ? Despite the evidence of 30 years and the  accompanying great and skillful actions of yours ?
      * Is that the reason why you beg ? And you suddenly thought that the only reasonable approach is to beg for what you shouldnt have done so that you can enjoy ?
+     * 
+     * are you still begging ?
       **/     
 
 
