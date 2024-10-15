d4701968e832c0ab6f7fb843ea476940e1f1010b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:12:43 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5341,6 +5341,7 @@ int main()
      * 
      * Are you still telling the same narrative ? I do not accept no animals like you
      * But do you see the problem right ? Okay okay, now talk about the "past" and "violence" and correlation between your inferiority seeing it as education ?
+     * I say irrelevant immature irresponsible ignorant monkeys, what do you say ?
       **/     
 
 
