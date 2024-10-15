73264a804a14250ba18273b2aaf589d4ce0b2548
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 08:04:44 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5421,6 +5421,8 @@ int main()
      * We are sorry!! we are sorry!! but we would like to do it more !! if you lie for us!! get women!!
      * But you see the sorts of things you enjoy right ? Your acting of "its job!!", "its obligatory!" does not change the fact
      * that you are intentional in all of your doings so far, so but do you see the problem now ?
+     * 
+     * Or you would rather beg for lies and take that for a win ?
       **/     
 
 
