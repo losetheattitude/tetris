f6969b6e3f23502bada52a2be4d27db5b5f0bf4b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:21:29 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5352,6 +5352,7 @@ int main()
      * Thank you for helping us improving on our experiences while you get harmed for no apparent reason!!!!
      * Is that the nonanimals ?
      * And you still think saying a few good things and offering what is most normal thing on earth should be enough ?
+     * But you also think that offering is just enough eheheh ? Thats the pattern right ?
       **/     
 
 
