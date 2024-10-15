2bbec09f31b192023c44385e7f57a76b14dbeccb
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:22:55 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5353,6 +5353,7 @@ int main()
      * Is that the nonanimals ?
      * And you still think saying a few good things and offering what is most normal thing on earth should be enough ?
      * But you also think that offering is just enough eheheh ? Thats the pattern right ?
+     * You also take coercion and violence for a skill, yes yes yes, thanks but I will pass the kinds of animals like you
       **/     
 
 
