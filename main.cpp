681cb93c42891108da007d60587d05147e974d88
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:07:28 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5334,6 +5334,8 @@ int main()
      * But do you see the stupid animals who get themselves into needless problems ?
      * Arent you the selfish immature irresponsible monkeys ? What do you have to say other than, "EHEHEH we just do it!!!", "violence!! hatred!!" ?
      * To a kid that you do not know ? and afterwards of which you can not even begin to elaborate ?
+     * 
+     * And you still offer materials as you have do so for the past 30 years ?
       **/     
 
 
