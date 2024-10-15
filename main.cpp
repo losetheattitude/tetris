cc925b064f979620f05f33175c48e9257af7c055
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-15 05:30:38 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -5368,6 +5368,7 @@ int main()
      * 
      * So you still are not inclined to see that you CAN NOT ENFORCE anything on nobody, but you would just like to hide that you have done so ?
      * that you have such and such inclanations ? Despite the evidence of 30 years and the  accompanying great and skillful actions of yours ?
+     * Is that the reason why you beg ?
       **/     
 
 
