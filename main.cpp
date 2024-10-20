a0c6d4a750055ca666c799d2ab1dd5625332881a
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-20 15:02:53 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -38,7 +38,10 @@ int main()
      * and you would also prefer ignoring what has been constantly told you that you are not actually achieving anything and continue acting like a stubborn disgusting monkey
      * who knows his guilt is unforgivable ? and you would also allege that you should make decisions ehehe such a funny thing 
      * and you would also love to show off your incredible interaction skills to the society through matters that are not your concern ?
+     * 
+     * and you would also like to ignore what has been told to you multiple times that "YOU ARE NOT RELEVANT AND I DO NOT WANT TO HEAR ANYTHING YOU COULD SAY ?"
       **/     
+     
 
 
 
