cb00417c283e3fb5dee8f3a1e7e8309932b3c814
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-10-20 16:06:34 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -60,6 +60,7 @@ int main()
      * Please we want harm!! lie!! forget the past!!! please! and you offer women you say ? 
      * Talk more about violence and religion and virtue eheheh also some intelligence then proceed with your beggings
      * Talk a little bit more about VIOLENCE I say, what do you say ? forget about the past!! please!! please!!! forget about the past!!
+     * I say irresponsible immature monkeys ? What do you say ?
       **/     
      
 
