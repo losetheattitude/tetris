bda90ab23b52ae50092a33e68d315059c8060b49
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-02 21:43:19 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -53,6 +53,7 @@ int main()
      * Most skillful indeed, we re your family!!!! plss family!!!! eheheh, we represent god!! we good religion ehhehehe pls pls 
      * most skillful indeed you can decide to harm, fucking dumb animals
      * most skillful as well that you think you should make irrelevant decisions because you are some sort of religious entity ehehehhe 
+     * eheheh do you love your life ? just as I thought what you are, you are a desperate fearful animal who takes violence for threat or education, what a correlation eheh
       **/     
 
      
