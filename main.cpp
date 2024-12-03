2e020fb54e9d97dd5ae9a26a16b0869a49d52496
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-04 01:41:19 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -68,7 +68,7 @@ int main()
      * not the homo sapiens who think they play games until they are told somehow THEY ARE WRONG
      * not the homo sapiens who think playing nonexistent stupid games over others lifes is "badass" and people are afraid because of it, guess what ?
      * not the homo sapiens who disthink that they do not enjoy but its a necessity ehhehehe I am definitely relevant to whatever you are doing as you are not animals
-     * not the homo sapiens who can not predict what their actions would entail, the sapiens who delude on what they have caused is "GOOD", whats my experience like ? eheheh
+     * not the homo sapiens who can not predict what their   would entail, the sapiens who delude on what they have caused is "GOOD", whats my experience like ? eheheh
      * Thats most useful for everyone, the same sapiens who take everyone for themselves
      * the sapiens who appear and act entirely childish ehhehehe, pls pls pls 
      * the very same sapiens who their superior would not even consider wasting one second with them, take a look at the history assess your stupidity
@@ -107,6 +107,9 @@ int main()
      * Were you what ?  Eheheheh we take it baack!!! ehehhe pls pls 
      * eheheheh medicine btw, eheheh medicine pls !! eehhhehe good ppl
      * I want no disgusting animals around me
+     * 
+     * 
+     * I HAVE NO INTENTION TO WASTE MY TIME WITH WHATEVER YOU COULD POSSIBlY FORM ? Do you even follow great religion and great nation and great people ?
       **/      
 
      
