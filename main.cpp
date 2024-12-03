d7f49bb2f9cc0a6bb2d2047bd547ce045830a0f9
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-04 02:15:22 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -115,7 +115,7 @@ int main()
      
      * You still do not understand the part where what you would not consider baring IS GOING TO HAPPEN ? Awww, that would be too bad for you all ? Why anyone should care ?
      * eheheh you are the one who is offering materials still ? see the problem ? See the problem in your understanding that IT DOES NOT HAVE ANY EFFECT ON ME ? Why ?
-     * DO YOU SEE THE PROBLEM BRO ? eheh anyways still wasting should be reasonable and efficient
+     * DO YOU SEE THE PROBLEM BRO ? eheh anyways still wasting should be reasonable and efficient, thats the uncontrollable and unaccountable people
       **/      
 
      
