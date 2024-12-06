dba94bc5d409af4bb612be6d9524644fc809d01b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-07 01:04:22 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -60,6 +60,7 @@ int main()
      * You see the problem now ?
      * 
      * Do you want to remember the past ? I EVEN REMEMBER THE MOST INSIGNIFICANT PART I SAY, WHAT DO YOU SAY ?
+     * YOU STILL DELUDE ON YOUR NATION ? YOUR PROFICIENCY ? YOUR USEFULNESS ? YOUR REASON ?
       **/      
 
      
