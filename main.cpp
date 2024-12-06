6217c9d25dcdc356162f403912efdca5d4bba0a9
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-07 01:09:50 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -72,6 +72,7 @@ int main()
      * Like the average animal ?
      * 
      * Is your really really genius idea is to offer women ? Hoping that it would entirely solve a fabricated problem from the beginning ?
+     * Disgusting you are not you say ?
       **/      
 
      
