c35ad789c56465218386619e7e368b4440512f44
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-07 17:59:34 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -202,6 +202,7 @@ int main()
      * Awww, you would rather offer women and hope that it solves the violence problem, the problem of life that you have zero understanding
      * eheheh call it a game, life a game!!! life a simulation!!! 
      * You never learn ? You as the disgusting homo sapiens ?
+     * You still offer women and just wish that it should work ?
       **/      
 
 
