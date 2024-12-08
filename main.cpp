349e1d39f6b4ad2a86b6c8db6489828822b6ad18
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-08 23:16:18 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -475,6 +475,7 @@ int main()
      * How about you start accounting for your own behavior then we talk about how to solve the rest ?
      * How about you realize you do not make that decision and pay for your own actions ?
      * How about you talk about your credibility before you ask things from someone who you harm intentionally ?
+     * Listen how to learn then learn how to listen ?
       **/       
 
 
