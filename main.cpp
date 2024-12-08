77a577b593b974cd296dd9c1ead015f3fe86b72b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-08 03:34:27 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -300,6 +300,8 @@ int main()
      * Need more disgusting excuses that only your minds can produce and believe at the same time ?
      * Dont you still understand that you are chasing money because you are doing what you want ? Do you think I care slighest about the money ?
      * You disgusting animals ? Will you start accounting for your disgusting behavior ? or start learning how to listen to people ?
+     * 
+     * Awwww, you are so deep in your disgusting animality you say ?
       **/       
 
 
