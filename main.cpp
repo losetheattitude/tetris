2867a26dc0dacbc75a6da8b2c320128997acc302
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-09 19:54:16 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -710,6 +710,8 @@ int main()
      * 
      * Do you feel gratified winning over such a thing ? Do you want to talk more about the latter ?
      * Do you still think you are playing games ? Wait a little harm falls upon you
+     * 
+     * WHAT ARE YOU WAITING FOR ?
       **/       
 
 
