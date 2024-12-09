938dadf92456f6c9be1d9709be73c0200909f0d1
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-09 19:10:46 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -675,8 +675,9 @@ int main()
      * 
      * You do not understand I am trying to live my life ? ehehehehheheheh FUN STUFF!!! GET THE FUCK OUT OF MY FACE
      * 
-     * Start explaining your disgusting actions 
+     * Start explaining your disgusting actions along with the "lifestyle" I had ?
       **/       
+
 
 
 
