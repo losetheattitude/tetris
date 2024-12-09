4fc9b2de75ced0158372eb6ea6e3f330432c41b2
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-09 16:02:14 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -589,6 +589,8 @@ int main()
      * 
      * Still ignoring the truth because you want to make "some gains" ? You got evidence but you would rather GAIN THINGS from others 
      * you intentionally harm and you talk about genius ?
+     * 
+     * I do not want to see no average sapiens around me
       **/       
 
 
