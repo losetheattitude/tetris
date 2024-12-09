0a548ba5bc80f5da4d8b7404cea7689b8fb9e600
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-09 19:47:25 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -704,6 +704,8 @@ int main()
      * Are you still making offers ? WHAT ARE YOU WAITING FOR ?
      * Do you even understand what you are doing or you just want to harm people ?
      * still hiding behind your disgusting excuses ?
+     * 
+     * Which part exactly is the funniest for you ? 
       **/       
 
 
