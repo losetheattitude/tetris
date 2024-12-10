1b811e5ab9d168bffdf3935150329655588ffe10
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-10 18:26:21 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -887,6 +887,7 @@ int main()
      * How about you get yourselves more children ? because your reason can only match with dumb monkeys ? How about you start talking about facts ?
      * 
      * Do you see the disgusting part ? awwwww, it must be so fun!!! Ehehehe its obligatory!!!! please you do not be like us!!!
+     * How about you pay for your own actions first ? then talk about being reasonable or creative ? Or just ?
       **/       
 
 
