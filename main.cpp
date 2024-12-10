8a058b31943087ae0967bfe7c551dd815eafb850
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-10 18:16:26 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -876,6 +876,7 @@ int main()
      * 
      * You still delude on its being and will being good ? eheheh it just does not matter so long we are content ? Ignore and ignore then what do you get ?
      * awwwwww, who knows ??? Still after illegal activies you take for being badass ?
+     * Still after offering materials over time with violence ?
       **/       
 
 
