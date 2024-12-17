d53306a7f11733ba4b0d5b4439e5e1b2c3c4a08b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-18 00:59:09 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -270,6 +270,8 @@ int main()
      * Still asking for the unspeakable ? why do you think you are a disgusting animal who is afraid of its surroundings ?
      * Forget about the past that is not our past thats why we can easily ask such things ? is that what you are saying ?
      * How about you gather more children ? and try to oppose them with threats ?
+     * 
+     * The disgusting selfish people of the earth ?
       **/       
 
 
