4ab7e1c842240c6c270d8deb44445187773beecd
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-18 04:45:37 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -360,6 +360,7 @@ int main()
      * I do not even want a second around any of you, you can live your life from the most possible distance away
      * 
      * I do not respect your decisions until you account for your own actions and not taking cheap lying for a skill
+     * Disgusting monkeys indeed
       **/       
 
 
