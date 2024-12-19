db2e511ea58e73e7b09caafeecd5086427444368
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-19 23:52:54 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -472,6 +472,7 @@ int main()
      * Does that sound complicated or would you like to ignore ?
      * 
      * Still deluding on others' demise ? awwwww thats why you insist otherwise you look like the most disgusting stupid animal on earth right ?
+     * The disgusting animals who still offer material for their irrelevant and unjustified decisions
       **/       
 
 
