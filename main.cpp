e0aa9bc6779e22fd3b522ce8c4dfc34163d48a5d
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-19 06:23:46 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -436,6 +436,8 @@ int main()
      * You do not see that everyone has his own life or you are just the animal that has no understanding whatsoever ?
      * Guess what you deserve ? Are you still making offers ?
      * I want neither good nor bad things from the very disgusting animals who I know what they are
+     * 
+     * I am not interested in satisfying "your obscure aims" that leads to nowhere, is that what you are not perceiving ?
       **/       
 
 
