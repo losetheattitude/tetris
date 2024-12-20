fdacef973ceed594d5b81b21b9a46ab1bf650782
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-20 03:34:41 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -483,7 +483,7 @@ int main()
      * 
      * I am not interested in being around disgusting animals, thats the only thing you can hear
      * The people who dislisten, I do not want to see you around me is only thing you can hear
-     * The people without a proper explanation
+     * The people without a proper explanation other than cheap lies as their nature can produce and some offers that they crave for
       **/       
 
 
