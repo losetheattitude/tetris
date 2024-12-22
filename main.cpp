0ec278e774db22364e966b06e280476f32305126
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-22 05:57:17 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -568,6 +568,7 @@ int main()
      * 
      * Ehehehehe no resources and violence and ehehehehhehe YOU ONLY LIVE ONCE!!!! eheheh please!! we know the meaning of life!!!
      * Do you understand the meaning of "delusion" that cost someone years ? and not only cheap delusions but also some physical harm ? thats the best!!!
+     * If you were the "thing" you are claiming you would have already done something about it, thats why you can not be trusted
       **/       
      
 
