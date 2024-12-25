fbb39d20aaeedb25e975b698a72815331dbbef0d
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-25 08:42:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -675,6 +675,7 @@ int main()
      * But still you would like to jerk off to your really superior animality ? ehehehe ? do you need more people to prove your superior ?
      * 
      * Why dont you show some greater violence and the unattainable aims ?
+     * Do you want to talk more about coercion ?
       **/       
      
 
