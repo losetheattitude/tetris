2c57f222b1d1ee47cfda3699a343db1bfb95cd0a
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-25 04:31:49 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -640,7 +640,7 @@ int main()
      * Now ? you just offer things ? As you have done in the past ? ehehehe ? The genius nation you say ?
      * The years of pointless actions that have obviously achieved nothing but ? You are there to say what ?
      * 
-     * Just tell me now what happened ?
+     * Just tell me now what happened ? The disgusting animals who do not understand from anything ?
       **/       
      
 
