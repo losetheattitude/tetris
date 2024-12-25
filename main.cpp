2e26a9f1a3816ef2d3c3d1e4d6fb03012f8a8c13
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-25 04:11:07 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -609,7 +609,7 @@ int main()
      * 
      * Awwwwww, itsss tooo latee!!!! Just say some good stuff and wait for things to settle!!!! please!! genius idea!!!
      * Do you even understand what I am saying ? I do not want to see any of you around me ? Do you follow ?
-     * Are you still making offers ?
+     * Are you still making offers ? Are you still making offers and ignore them as your disgusting nature allows you to ?
       **/       
      
 
