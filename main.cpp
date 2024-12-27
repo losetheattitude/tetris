e1cdd3c8121b5a095eb1ad9f6633f7d89dc6ff46
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-27 05:58:11 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -736,6 +736,7 @@ int main()
      * So all those years what have you achieved ? ehehhehe other than some inferior coercion ?
      * More coercion because you have no other things to do ?
      * How about you make decisions only for your own life instead of ignoring the outcomes of your actions ?
+     * You do not realize your failure after 30 years of random violence and actions without direction ?
       **/       
      
 
