a7f3a517d2a919eb38c22992ccbddb7051187afc
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2024-12-31 13:44:45 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -867,6 +867,7 @@ int main()
      * 
      * How about you get more children to beg and be proud about later on ? and ignore all the things you have done and said ?
      * No no, how about more medicine ? Guess who is not wanted ? Do you even have a brain ? or you would just like to ignore ?
+     * How about you do that within your vicinitiy ?
       **/       
 
 
