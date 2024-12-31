ecba7efc972bf102f026b2a53e733f6727573992
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 01:28:29 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -952,7 +952,7 @@ int main()
      * eehehe ignorance you dont say ? You lack the single thing I value, credibility or how about more medicine ? eheh coercion solves everything
      * 
      * Do you only like to say things and do nothing about them ?
-     * After decades you are still the disgusting animal ?
+     * After decades you are still the disgusting animal ? Who is deluding do you think ?
       **/       
 
 
