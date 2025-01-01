788254415f5d021a99e3df58c0b65e73893492ed
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 19:10:46 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1100,6 +1100,7 @@ int main()
      * To waste another 30 years to arrive nowhere but complete harm ?
      * Why dont you go out and seek for more children so that you can feel great about yourselves ?
      * The genius nation ? Waiting for another 30 years to waste or just gather 123213921 people and harm ?
+     * What does sound too complicated for you ? That you are an inferior disgusting animal ? who is who ? Who is ignorant ?
       **/       
 
 
