40841998e8b746d3ebb4ab7d3903ef9825359b80
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 17:17:26 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1059,6 +1059,7 @@ int main()
      * I do not care any false precept you have convinced yourselves with, I am not interested 
      * 
      * I am not interested in wasting my time with you neither do I want to assist any of your disgusting purposes
+     * Who do you think is afraid ?
       **/       
 
 
