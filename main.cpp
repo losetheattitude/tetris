df1249efb4a9e0450fe77c8a2917c7de6cef8e6d
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 22:39:49 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1217,6 +1217,8 @@ int main()
      * Who do you think IS GUILTY for wasting such a time with pointless actions ? 
      * Who do you think is guilty in intentionally not providing even the most essential resources ?
      * Arent you the disgusting little animals still ? Why are you still asking things ? 
+     * 
+     * Why are you still asking for lies, the disgusting little animals ?
       **/       
 
 
