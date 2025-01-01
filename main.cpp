ac83b6a2cef0fe7802927dfaaa90cd07255a9b64
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 16:58:51 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1054,6 +1054,7 @@ int main()
      * The same disgusting animality that coerces children to do unspeakable things so that they could provide it as reason later on ?
      * 
      * Do you have any explanation or would you like to ignore through incompetent and irrational acting that you take for a skill ?
+     * What are you waiting for ?
       **/       
 
 
