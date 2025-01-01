fbfa7edb54ae8e86a860559ef4d0cc2f7ab60e5a
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-01 20:03:39 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1136,6 +1136,8 @@ int main()
      * When all you have to do is so simple yet look at your stupid decisions, do you think you deserve respect ?
      * Do you think you deserve respect or you are just delusional ? Its so simple what you have to do yet look at the games you are playing ?
      * Did you delude that you are genius nation also ?
+     * 
+     * All you have to do is so simple yet you are still wasting your time around ?
       **/       
 
 
