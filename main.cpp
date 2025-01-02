63b6c3b6cae75ff9e765bc80de1d5948c5cb0bd1
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-02 16:55:00 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1349,7 +1349,7 @@ int main()
      * I am sure as genius nation you can guess under any circumstances whatsoever will I ever consider any of the disgusting animals around me
      * 
      * You can continue deluding without me, I am not interested
-     * Guess what is the reason ? 
+     * Guess what is the reason ? Does great people can understand what they have achieved or just trying to live their lives to the fullest extent ? eheheh?
       **/
 
 
