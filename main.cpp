5a34fa9936fa60a3a36657d585402ca2cf3d3c3c
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-03 18:34:03 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1466,6 +1466,7 @@ int main()
      * There is really nothing you could do to change that perspective you have especially carved on your own
      * 
      * What makes you think I have even the most tiniest irrelevant attention for you ?
+     * You are not welcome
      * 
       **/ 
 
