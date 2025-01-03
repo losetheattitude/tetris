b34a940bb18522b1d34ce59e520b9f1252efed64
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-03 13:26:40 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -1391,6 +1391,8 @@ int main()
      * How about you pay for your own actions and deal with your own problems ? Delusional genius nation ?
      * How about you learn some rules first ? Instead of offering women ? Like handling your own problems before
      * you be a problem for others ? Guess who is not wanted despite the really great titles they hold ?
+     * 
+     * Do the great titled people need some education on the rules ?
       **/
 
 
