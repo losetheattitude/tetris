2309146cc7f0a54958b85986649237cd06cffff0
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-04 20:39:50 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -139,6 +139,7 @@ int main()
      * What a shame you are still a disgusting monkey who offers the most basic material which should have been already there!
      * 
      * Making the mistake of "guessing" miserably that I will start suddenly begging like you do ?
+     * How about some medicine ? eheh ?
       **/ 
 
 
