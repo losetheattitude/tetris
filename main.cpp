eced4ee27cca70318b54dcf8396a335f2ec6cbea
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-04 14:58:36 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -57,6 +57,7 @@ int main()
      * Intelligent and developed ehehehe and credible hehehehe smart genius nation ehehehehe
      * Definitely what you also delude yourselves to be, eheheh
      * lets gather 10230012 people and assault single individual ehehhehehe, intelligent and developed and eheheheheh genius eheheh
+     * Most scary stuff 
      * 
       **/ 
 
