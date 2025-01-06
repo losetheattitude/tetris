a7c6836e3ffcecb124d8deca9d8762815859f8e0
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-06 18:03:11 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -237,6 +237,7 @@ int main()
      * Eheheheh life is not fair heehheheheehhe life simulation eheheheh genius nation eheheheheh handling children real good eheheh
      * 
      * Why dont you come back at your "mistake" and think it through ehehehehehe genius btw
+     * Ehehe we like harming and making false offers ehehehe
       **/ 
 
 
