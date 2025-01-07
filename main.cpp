1202a4a24f787d6f0a1ebf86e6aaba6e54e67fbc
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-07 18:04:21 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -262,6 +262,7 @@ int main()
      * Guess who is waiting for nothing ? Eheheheheheheheheheh its just life, we like enjoying ours!!
      * Guess who is a ignorant monkey who never learns
      * Guess who is trying their hardest to hide what they have perpetrated
+     * Guess who is not going to listen to you as you have done so ?
       **/ 
 
 
