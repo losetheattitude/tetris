d15b227648e2f78d044ec42d811e14ef4d962e80
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-08 15:41:35 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -313,6 +313,7 @@ int main()
      * Save your assumptions to yourselves, do you understand what is spoken language ? 
      * 
      * Eheheheheh we managed to unattainable eheheh wasting time that does not belong to us hehehe
+     * Having fun killing people ?
       **/ 
 
 
