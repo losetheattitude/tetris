b5784d0c7746f0ec301cb29d677b51045c078cff
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-09 03:45:17 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -367,6 +367,8 @@ int main()
      * Find the worst examples of the worst, so that you can feel okay, thats the definition of disgusting desperate people
      * Guess who is not interested in stupid people who likes to make up excuses for their own experiences ?
      * Guess who hates to interact with the kinds of people like you ?
+     * 
+     * Guess who is guilty and does know his guilt thus has to beg still 
       **/ 
 
 
