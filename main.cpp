ab6fd473cd45e4188f230b2662b08a71eee9c060
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-12 01:30:45 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -92,6 +92,8 @@ int main()
      * What a shame that you are still proud or sure on your devices, guess who is not wanted despite their really important resources that they wield as weapon
      * 
      * Waste your own time while knowing that you are the one who has crossed the every single personal boundary, therefore the disgusting ignorant animal
+     * 
+     * Guess who is not interested in your fake attitude ? Say some nice stuff its fine since its not our time that we destroy
       **/
 
 
