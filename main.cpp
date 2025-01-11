45f3db723c873db8cf1e6df261f191a23107c424
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-12 01:18:08 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -90,6 +90,8 @@ int main()
      * 
      * You will guess nothing will change as well, just get the fuck out of my face I am not interested
      * What a shame that you are still proud or sure on your devices, guess who is not wanted despite their really important resources that they wield as weapon
+     * 
+     * Waste your own time
       **/
 
 
