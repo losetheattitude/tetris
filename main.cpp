501cfddc7a2a54d0982c80b490159612c7cd957f
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-12 02:37:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ int main()
      * Thats why you are a little inferior animal
      * Hide like the disgusting ignorant animal that you are
      * 
-     * Sacrifice your own resouces next time 
+     * Sacrifice your own resouces next time, like I said, little animals
       **/
 
 
