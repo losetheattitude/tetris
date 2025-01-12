5da698bcdd7e38020a6dd714ec2f122e658d3a36
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-12 15:41:55 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -173,6 +173,7 @@ int main()
      * 
      * YOU CAN NEVER BE WELCOME, guess who is failing ?
      * Awwwwww itss tooo late!!!! ehehhehe we would rather enjoy our experiences ignorantly anyway!!
+     * And most importantly, we offer materials our disgusting nature love to have when we are in a tough spot and know that we are guilty
       **/
 
 
