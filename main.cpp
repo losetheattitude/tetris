9b9d97a5e39e5977c7f6b546c8d534f519bff9cf
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-14 21:07:24 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -164,6 +164,7 @@ int main()
      * What a success!!!! Look at the outcome!!!
      * 
      * The outcome of decades of violence and lack of resources, is just off the roof!!!
+     * Guess the immature hehehehe but also the ignorant of the reality ? eheheh Have you grown up yet ?
       **/ 
 
  
