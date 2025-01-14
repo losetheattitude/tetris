b8ce815ba833992d167d318aa39c42415eee5c01
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-14 03:28:10 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -87,6 +87,7 @@ int main()
      * Whats wrong ?
      * 
      * Guess who is still crossing personal boundaries and asking things from people they commit ?
+     * Awwwww, havent you forgotten ? Ehehehehe why are you still making promises the joint genius nation ?
       **/ 
 
  
