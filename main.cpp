42db6038a5bfcaa97e949d21ca7eac4f699a5d2b
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-01-15 12:48:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -329,6 +329,8 @@ int main()
      * 
      * In what part are lacking resources and violence helping ?
      * You better sacrifice from your own, then you may begin to understand
+     * 
+     * Now is the time where you account for not your decisions
       **/ 
 
  
