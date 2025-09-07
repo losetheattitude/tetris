8ae5310a65c1d1f7e5c73322366118aedaa5c520
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-09-07 20:31:15 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -16,127 +16,129 @@
 #include "src/exchanger/exchanger.hpp"
 #include "src/levels/level.hpp"
 #include "src/formatter/formatter.hpp"
- 
+
 #include "src/core/joystick.hpp"
-    
-int main() 
+
+int main()
 {
     /**
-     * 
+     *
      * Event dispatching for Game class to further decouple and facilitate the transmission of internal properties otherwise
      * unamicable to perform so (Composition or inheritance(IExchanger like) ???)
-     * Async environment for RemoteTerminal and RemoteStorages consider Disruptor for advancing purposes 
+     * Async environment for RemoteTerminal and RemoteStorages consider Disruptor for advancing purposes
      *
-     * 
+     *
      * Waaaowww, what a great experience and opportunity !!! go galsss forever ayyy
-     * 
+     *
      * Ayyyyyy, you have done it !!!! thats success
-     * 
+     *
      * HIGHWAYYYY AYYYYYYY, disgusting inferior animals
      * Ayyyyy, makin decisions aaayyyayayaya
-     * 
+     *
      * Codin codin makaveli genius eu coungtry ayyy ahahahha
      * Ayyyyyyy making the impossible ayayayay the western genius ayayaya the western wayy awwwwwww goood jobb !! go galss for avar !!! yaaaa
-     * 
-     * 
-     * 
-     * 
+     *
+     *
+     *
+     *
      * Ayayayayay definitevly the best of the one of the best, you all have no competitors goood really goood fooling !!! ayyy
      * Youcan get the fuck out of my face INDEFINITELY!
-     * 
+     *
      * THE BESTTT AYYYYY
-     * 
+     *
      * Offferin muney muney wumen wumen !! ayayaya good job !! the advanced ppl saviors
      * Looooking at the results ayayayay, you have done a wondeful jbo the good county eu !!! ayyyy
-     * 
+     *
      * Makin great genius moves decisions ayayayaay
-     * 
+     *
      * The really reallly well knowing what they do people !!! genius eu !!
-     * 
+     *
      * Foooling fooling eu foolerss ayayaya good job !!! good eu ppl !!! genius engineerings
      * TRY HARDER YAYYYYY
-     * 
+     *
      * Years and years of experience ayayayay building things good people EU good coutnry!! genius engineeringss ayyyyy
-     * 
-     * 
-     *  
+     *
+     *
+     *
      * DA BEST OF DA BEST GENIUS COUNTRY PPL EU !!!  Definitely making reall realll goood stuff !!
      * Disgusting animals who are wasting ppl time ayyyyyyyy  !!! good stuff
-     * 
-     * Foolin foolingggg AYAYY done the best deal of the best !!! engineeringsss 
-     * 
+     *
+     * Foolin foolingggg AYAYY done the best deal of the best !!! engineeringsss
+     *
      * DISGUSTING DELUSIONAL ANIMALS can keep enjoying their lives !!
-     * 
+     *
      * DAAAAA BIGGESTTTT GENIUS COMMUNUITY THE EUUU MAKING THE BEST DECISIONS THAT DO NOT BELONG TO THEM AYYYYYYYYYYYYY
      * GO GALSS FORAVARRRR !! YAYYY
-     * 
-     * 
+     *
+     *
      * THE EU ANIMALS ayyyy go galss !!
-     * 
+     *
      * DA GREAT EU COUNTRY MKAING MARBLES !!! ayyyyyyyyyyyy
-     * 
+     *
      * Ayayayay da greates of the greates the EU makin real big plays on the children ayayayaya
-     * 
+     *
      * Disgusting little animals are still doing their best to ruin !!! ayyyyyyyyyy what a good country !!! gooooood inteligence !!! go galss!!
-     * 
+     *
      * Little disgusting inferior animals ? AWWWW you do not see ?  ayyyyyy what a genius great community ayayayaya
-     * 
+     *
      * Disgusting littlee eu animals making great content for other people to enjoy !!! ayyyyy
-     * 
-     * 
+     *
+     *
      * THE INCOMPARABLE OUTCOME AYYYYYYYYYYYYYYYYYYYYYYYYYYYYY THE GREATEST OF THE GREATES EU COMMUNITY AYyYYYY
      * Making eveything x10000 worse aaayyyyyyy OMGGG !!!!! the real deal genius ppl !!
      * Offering materials muney muney !!! wumen !!! ayyy
-     * 
-     * 
+     *
+     *
      * Ayyyyy the best of the best !! go gogogogo gals you da best !!!
-     * 
+     *
      * The real intellligent community doing the most work !!! ayyy the best outcome !!
-     * 
+     *
      * Making the biggest of the biggest bullshit ever !!!!! awww makin problemsss when there no problems ayyys
-     * 
+     *
      * Foolin foolin fooliin awww ayyyyy intelligent!!! ayyy
      * Da best of the best genius engineering humans savioring the universe !!!! ayyyyy precious lives !!! ayyyy eheheheh good job !!
-     * 
-     * 
+     *
+     *
      * Da biggest genius making impossible things !!! really really beneficial genius engineerings ayyy
      * What a great outcome by the biggest geniuses of our precious universe making everything possible ayyayayaaa !!! goo galss !!
-     * 
-     * 
+     *
+     *
      * Da great building by the very very genius community of the unverse the SAVIORS !!! ayyy guess what ???  GOOO GALSSS FORAVARRR !! yayyay
      * genus s!!!
-     * 
+     *
      * Little disgusting inferiors acting like its okay !!!!!!!!!!
-     * 
+     *
      * Just made a wondaful thing into the most disgusting situation ever !! WHAT A GENIUS YALL AREE THE EU !! TBHE GREAT CoUnTrY usa !!! ayyyy
-     * 
+     *
      * GO GALSS FOREVERRR  !!! And offer muneyy muneyy materialas carsss ayyy ayayayayayayay  wumen !!! the biiggest geniuses of the world !!!
      * unvierse yayyy life !!! life a simnujkaltion !! ayy6 goooo !!
-    */
+     *
+     * ITS DONE !!! AYYYY ITSS DONEEE !! 
+     */
 
-    InitWindow(500, 620, "Raylib Tetris");  
+    InitWindow(500, 620, "Raylib Tetris");
     SetTargetFPS(60);
 
     // Grid* grid = new Grid();
     // Level* level = makeLevelSingleton(2);
-    // InputTerminal* terminal = n ew InputTerminal();    
+    // InputTerminal* terminal = n ew InputTerminal();
 
-    Grid* grid = new Grid();
-    Level* level = makeLevelSingleton(2);
- 
-    // BaseStorage storage = BaseStorage(); 
+    Grid *grid = new Grid();
+    Level *level = makeLevelSingleton(2);
+
+    // BaseStorage storage = BaseStorage();
     // BaseTransformer formatter = BaseTransformer();
     // Exchanger exchanger = Exchanger(&storage, &formatter);
- 
-    Game* game = new Game(grid, level, 0);
+
+    Game *game = new Game(grid, level, 0);
     CommandFactory::Init(*game);
 
-    Terminal* terminal = new LocalTerminal();
-    JoyStick* joyStick = new JoyStick(terminal);
+    Terminal *terminal = new LocalTerminal();
+    JoyStick *joyStick = new JoyStick(terminal);
 
     GameScene gameScene = GameScene(game, joyStick);
-    while(!WindowShouldClose())
-    {  
+    while (!WindowShouldClose())
+    {
         BeginDrawing();
         gameScene.Draw();
 
