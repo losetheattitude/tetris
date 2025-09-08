4e9841822bc5cd9cdc9b05909693333a075e60cc
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-09-09 02:06:04 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -27,53 +27,7 @@ int main()
      * unamicable to perform so (Composition or inheritance(IExchanger like) ???)
      * Async environment for RemoteTerminal and RemoteStorages consider Disruptor for advancing purposes
      * 
-     * MAKIN DECISIONS AYAY MAKIN PERFECT ACTIONS AYAY WE DA BEST WEST AYYYY
-     * WHAT A LOVELY OUTCOME !!! DA BEST OF THE BEST AYYY MAKIN DECISION THEY WERE NOT ASKED FOR AYYYY
      * 
-     * WUMENNN CARSSS MUNEYY MUNEYYYY GET MUNEYYY WUMENNN WUMEN ENOUGH YAYYYY
-     * 
-     * DA BEST OF DA BEST EUROPAEN GREAT GUYS OFFFERING WUMEN WUMENNNN MUNEYYY CARSSS AYYYY 
-     * LIFE NOT FAIR ayayayay LIFE A GAME NOT A GAME AYYYYY
-     * 
-     * THE WHOLE KNOWING PERFECT CIVILIZATION DA WEST NUMBA ONE WEST OFFERING MUNEY MUNEYYYY WUMENNN CARSSS
-     * FOR VIOLENCE AYYYYYYYYY GENIUS !!! 
-     * 
-     * AGE DO NOT MATTER, TIME DOES NOT EXIST, WE PROFESSIONAL PSYCOLOHGISY AND PHSYICİTIY FROM THE WORLD 
-     * AYYAYY DA WESTTT NUMBA ONE !!!
-     * 
-     * DA BEST OF DA BEST ENGINEERINGS AND PSYCOLOGIST AND PHSYSCIST FROM DA WEST!!! MAKIN PERFECT ACTIONS !!!
-     * 
-     * Ayyyyyyyy !! numbe on !!!
-     * 
-     * THE WHOLE MIGHTY WISE CIVILIZATION FROM DA WORLD NUMBA ON IN THE UNIVERSE
-     * MAKIN DECISION AYYYYYY GREAT PSYCOLOGISYST AND PHSYICITS AND ENGINEERSS AYYYYYY
-     * 
-     * THE MIGHTY KNOWING PEOPLE FROM DA WILD WILD WEST WASTING OTHER PPL TIME WITHOUT HESITATION !!! 
-     * THE GENIUS NATION !!
-     * 
-     * The professional outcome builders !!!! the phsycisits and psycologisit and engineers !! ayyyyyyyyyy
-     * Ignoring evidence ayyyyyy genius !!! geniuss !!! numba one civilization !!!
-     * 
-     * 
-     * WAOOOWWW WHOLE DELUSIONAL MIGHTY CIVILIZATION BUILDERS !!! GROUP OF GENIUS PSYCOLOGIEST AND PHSYCITSY AND ENGINEERSS !!
-     * WHAT A CHANCE !!!! 
-     * OFFERIN WUMEN FOR UNSPEAKABLE AYYYY WUMENN MUNEYYYY CARR OMGGGGGG
-     * GO BACK TO YOUR DISGUSTING DELUSIONAL MOVIES AND SATIFY YOUR DISGUSTING BRAINS THE GENIUS NATION ACHIVING ayyyy
-     * 
-     * 
-     * DISGUSTING ANIMAL CAN CONTINUE DELUDING AND WATCHING SUPERFICIAL CONTENT MOVIES
-     * ALMIGHTY WESTERN CIVIZLATION MAKIN ASSUMPTIONS AND TRYING NOT TO LOOK STUPID !! AYYYYYYY
-     * GENIUS NATION !! OFFERIN MUNEY ADN WUMEN ! ayyyyyyywestern nnnnnn
-     * 
-     * Da numbe one assumption makers kids abuser and genius psyclogist and phsyciist also engineers 
-     * with delusional superficial movie makin creative brains !!!
-     * 
-     * WHAT AN OUTCOME !!!
-     * 
-     * I am not interested in THE SLIGHTEST thing you can offer
-     * 
-     * The whole mighty knowing advanced modern yet assumption maker great civilization s
-     * I will pass without a second thought
      * 
      * Almighty assumption making wise and all knowing immaculate great cultured civilization
      * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
@@ -84,6 +38,89 @@ int main()
      * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
      * 
      * What do you think it is ? Ayyyyy you have done it !!
+     * 
+     * 
+     * * Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!
+     * 
+     * * Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!* Almighty assumption making wise and all knowing immaculate great cultured civilization
+     * DA BEST OF DA BEST OF THE UNIVERSE, DA WEST !! WHO WOULDNT LOVE TO BE INVOLVED !!!
+     * 
+     * THE TRUE ROMANS AND GANGSTAS AYYYYY makin assumptions !!!
+     * 
+     * Offeringgg wumennn muneyyyyyy for the failed assumptions ayyyyyy
+     * What a great outcome built by the very genius phsysists and engineers and psycologists ayyyy
+     * 
+     * What do you think it is ? Ayyyyy you have done it !!
+     * 
+     * 
+     * 
+     * 
+     * 
+     * I WILL PASS WITHOUT HESITATION
      */
 
 
