b232f18e2fe92c429e1b65c192dea294b13d3ab6
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-10-06 14:48:47 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -144,7 +144,7 @@ int main()
      * Number one !!! the best !! very wise and clever people !!! assumptions and decisions !! That were not YOUR PLACE !!! GO GALS !! Da number one 
      * I am not interested in anything you can provide, go back to your delusions marvelous ppl
      * 
-     * Good reason!!! 
+     * Good reason!!! Perfect execution !!! very skilled and talented delusional ppl !! you can go away, I am not interested
     */ 
 
    
