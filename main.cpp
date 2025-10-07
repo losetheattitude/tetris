3053b059f4f23afe4da4901c6a7b35221e84ba8e
losetheattitude <114943337+losetheattitude@users.noreply.github.com>
2025-10-07 23:25:48 +0300

    asd

--- main.cpp
+++ main.cpp
@@ -425,6 +425,8 @@ int main()
      * Wumen wumennn people CAN GET THE FUCK OUT OF MY FACE
      * Go on to your disgusting hating and ignoring with the beatiful prejudiced disgusting minds of you
      * Good job!! Next time find a better candidate
+     * 
+     * NOT INTERESTED IN ANYTHING THAT COMES FROM YOU
      * */
     
    
